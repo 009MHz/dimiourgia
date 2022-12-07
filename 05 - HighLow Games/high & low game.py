81d144de157abcf33de872f3d0041a4128ab15da
009MHz <mharisbsatriyo@gmail.com>
2022-12-08 02:58:15 +0700

    HighLow Code refactoring

--- 05 - HighLow Games/high & low game.py
+++ 05 - HighLow Games/high & low game.py
@@ -1,30 +1,33 @@
-#0 importing all needed modules
 import os
 import random
 from art import logo, vs
 from game_data import data
-#1 Generate a random account from the game data. 
+
+
 def topics():
+    """Generate a random account from the game data."""
     return random.choice(data)
 
-#2 function to call the randomized data into printable format
+
 def details(topic):
+    """Function to convert randomized dictionary into printable format"""
     figure = topic['name']
     details = topic['description']
     city = topic['country']
-    # print (f'{figure}, a {details}, from {city}.')
+    # print (f'{figure}, a {details}, from {city}.')  # for debugging purpose
     return f'{figure}, a {details}, from {city}'
 
-#3 function to compare the answer
-def checker(answer,a_count,b_count):
+
+def checker(answer, a_count, b_count):
+    """User input validation"""
     if b_count > a_count:
         return answer == "b"
     else:
         return answer == "a"
 
-#4 Game Continuation
-    #create default parameter & inserting the logo
+
 def game():
+    """Game Continuation"""
     game_proceed = True
     print(logo)
     score = 0
@@ -40,17 +43,16 @@ def game():
         print(f'Compare A: {details(a_topic)}.')
         print(vs)
         print(f'Against B: {details(b_topic)}.')
-        #prompt to answer
+        # prompt to answer
         answer = input('Who has more follower? Type "A" or "B": ').lower()
         
-        #calling the value to use the compare function
+        # Comparing the data contents
         a_follower = a_topic["follower_count"]
         b_follower = b_topic["follower_count"]
-        correct_answer = checker(answer,a_follower,b_follower)
-        
-        #if statement to give the feedback to user
-        
+        correct_answer = checker(answer, a_follower, b_follower)
+
         os.system('clear')
+        # Scoring based on correct answer
         if correct_answer:
             score += 1
             print(f'BRAVO! Current score: "{score}".')
@@ -58,6 +60,7 @@ def game():
             game_proceed = False
             print(f'Incorrect! Your final score is: "{score}".')
             print(f'the "A" count is {a_follower} & the "B" count is {b_follower}')
+
 
 while input('\nType "y" to start the game, or type "n" to end the session? ').lower() == "y":
     game()
